e502cee97b5301da2e0b837dda67324dd65bf042
Erdaulet <erdauletmedetuli@gmail.com>
2024-10-10 11:20:43 +0500

    lab 4

--- demo/src/main/java/com/example/demo/repository/StudentRepository.java
+++ demo/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
+import java.util.*;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, UUID> {
